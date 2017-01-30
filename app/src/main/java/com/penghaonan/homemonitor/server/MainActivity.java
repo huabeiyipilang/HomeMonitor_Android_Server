ab278447d3ffaa9f0c601432657642963f250f65
Carl Li <huabeiyipilang@gmail.com>
2017-01-30 08:19:40 +0800

    权限检查

--- app/src/main/java/com/penghaonan/homemonitor/server/MainActivity.java
+++ app/src/main/java/com/penghaonan/homemonitor/server/MainActivity.java
@@ -1,17 +1,18 @@
 package com.penghaonan.homemonitor.server;
 
+import android.Manifest;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.penghaonan.homemonitor.server.command.Torch;
+import com.penghaonan.appframework.utils.CommonUtils;
 import com.penghaonan.homemonitor.server.manager.CommandManager;
-import com.penghaonan.homemonitor.server.messenger.AMessage;
-import com.penghaonan.homemonitor.server.messenger.Client;
-import com.penghaonan.homemonitor.server.messenger.TextMessage;
 import com.penghaonan.homemonitor.server.messenger.local.LocalMessengerAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements LocalMessengerAdapter.LocalMessageListener {
@@ -23,9 +24,20 @@ public class MainActivity extends AppCompatActivity implements LocalMessengerAda
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCmdInputView = (EditText)findViewById(R.id.cmd_input);
-        mOutputView = (TextView)findViewById(R.id.tv_output);
+        mCmdInputView = (EditText) findViewById(R.id.cmd_input);
+        mOutputView = (TextView) findViewById(R.id.tv_output);
         CommandManager.getInstance().requestPermissions(this);
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        List<String> permissons = new LinkedList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            permissons.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        permissons.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        permissons.add(Manifest.permission.CAMERA);
+        CommonUtils.checkPermission(this, permissons);
     }
 
     @Override
